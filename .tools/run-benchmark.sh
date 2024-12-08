1b8dba81701be4e1dc14aa95570dfda9958001bf
Sven Kanoldt <sven@d34dl0ck.me>
2024-12-08 22:17:21 +0100

    bit more robustness checks on the benchmark script

--- .tools/run-benchmark.sh
+++ .tools/run-benchmark.sh
@@ -7,13 +7,29 @@ set -u # exit on using unset variable
 SOLUTION_FOLDER=$1
 NAME=$(basename $SOLUTION_FOLDER)
 
+# IMPORTANT: all folders below are relative to the solution folder as root folder!
+
+BENCHMARK_DATASET=../samples/weather_1B.csv
+RESULTS_FOLDER=../guide/src/20_leaderboard/results
+
 # check that the provided folder exists and is a cargo project
 if [ ! -d $SOLUTION_FOLDER ]; then
     echo "The provided folder does not exist: $SOLUTION_FOLDER"
     exit 1
 fi
-
 cd $SOLUTION_FOLDER
+
+# check that the results folder exists, if not create it
+if [ ! -d $RESULTS_FOLDER ]; then
+    echo "[warn] The results folder does not exist: $RESULTS_FOLDER, creating it"
+    mkdir -p $RESULTS_FOLDER
+fi
+
+# check that the benchmark dataset of 1B rows exists
+if [ ! -f $BENCHMARK_DATASET ]; then
+    echo "The benchmark dataset of 1B rows does not exist"
+    exit 1
+fi
 
 # first build the solution
 cargo build --release
@@ -23,6 +39,6 @@ if [ $? -ne 0 ]; then
 fi
 
 hyperfine --warmup 0 --runs 5 \
-    --export-json ../guide/src/20_leaderboard/results/$NAME.json \
+    --export-json $RESULTS_FOLDER/$NAME.json \
     --show-output \
-    'cargo run --release -- ../samples/weather_1B.csv'
+    "cargo run --release -- $BENCHMARK_DATASET"
