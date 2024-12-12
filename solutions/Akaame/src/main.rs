cf92d8bf86d1ab43d02fb05175ff3ee657f7921b
Siddik Acil <sddkacil.1@gmail.com>
2024-12-12 22:07:25 +0100

    solutions(Akaame): Mmap?

--- solutions/Akaame/src/main.rs
+++ solutions/Akaame/src/main.rs
@@ -22,7 +22,7 @@ fn main() {
     // Read the input file using clap
     let args = Cli::parse();
     let input_file = args.input_file;
-    solution_bulk(input_file);
+    solution_mmap(input_file);
 }
 
 fn solution_dummy(input_file: String) {
@@ -71,6 +71,43 @@ fn solution_bulk(input_file: String) {
     // let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
     // let reader = BufReader::new(mmap.as_ref());
     let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let parts: Vec<&str> = line.split(";").collect();
+        let key_str = parts[0];
+        let value_str = parts[1]; // as float
+        let v = value_str.trim().parse::<f64>().unwrap();
+        solution.entry(key_str.to_owned()).or_insert(Vec::new()).push(v);
+    }
+
+    // Print the solution
+    println!("{{");
+    let mut iterable = solution.iter_mut().peekable();
+    while let Some(mut iter) = iterable.next() {
+        let key = iter.0;
+        let stats = iter.1;
+        stats.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        let length = stats.len();
+        // Calculate the mean
+        let sum: f64 = stats.iter().sum();
+        let mean = sum / length as f64;
+        let mut line = format!("\t{}={:.1}/{:.1}/{:.1}", key, stats[0], mean, stats[length-1]);
+        if iterable.peek().is_some() {
+            line += ",";
+        }
+        println!("{}", line);
+    }
+    println!("}}");
+}
+
+fn solution_mmap(input_file: String) {
+    // Create the solution hashmap
+    let mut solution: std::collections::BTreeMap<String, Vec<f64>> = std::collections::BTreeMap::new();
+
+    // Read the input file line by line
+    let file = OpenOptions::new().read(true).open(input_file).unwrap();
+    let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
+    let reader = BufReader::new(mmap.as_ref());
     for line in reader.lines() {
         let line = line.unwrap();
         let parts: Vec<&str> = line.split(";").collect();
